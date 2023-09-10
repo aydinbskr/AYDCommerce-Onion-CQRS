d2d89e9da1ad02cfee29bc4739c7dce4baf63826
aydinbskr <aydinbskr@gmail.com>
2023-09-10 19:02:24 +0300

    Entities are created

---
+++ Core/AYDCommerce.API.Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using AYDCommerce.API.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYDCommerce.API.Domain.Entities
+{
+    public class Brand:EntityBase
+    {
+        public required string Name { get; set; }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+    }
+}
