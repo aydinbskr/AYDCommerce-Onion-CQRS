abd748fb00be0bc2a0ecfa84b585316e91fb8ab2
aydinbskr <aydinbskr@gmail.com>
2023-09-24 21:54:26 +0300

    WriteRepository added.

--- Infrastructure/AYDCommerce.API.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/AYDCommerce.API.Persistence/Repositories/WriteRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AYDCommerce.API.Application.Interfaces.Repositories;
+using AYDCommerce.API.Domain.Common;
+using AYDCommerce.API.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,33 @@ using System.Threading.Tasks;
 
 namespace AYDCommerce.API.Persistence.Repositories
 {
-    internal class WriteRepository
+    internal class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _dbContext;
+        private DbSet<T> Table { get => _dbContext.Set<T>(); }
+        public WriteRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
     }
 }
