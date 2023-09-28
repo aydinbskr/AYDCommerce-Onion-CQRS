b0a3e8ef3386bbb05e3d262ed7d90cee2de828a0
aydinbskr <aydinbskr@gmail.com>
2023-09-28 23:23:28 +0300

    Mediatr added

---
+++ Presentation/AYDCommerce.API.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using AYDCommerce.API.Application.Features.Products.Queries;
+using AYDCommerce.API.Application.Interfaces.UnitOfWorks;
+using AYDCommerce.API.Domain.Entities;
+using Azure.Core;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYDCommerce.API.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            var response = await _mediator.Send(new GetAllProductsRequest());
+            return Ok(response);
+        }
+    }
+}
