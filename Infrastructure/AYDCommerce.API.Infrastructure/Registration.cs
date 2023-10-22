da417ebf3f1455d60d884e45fdd7ca81a6a87967
aydinbskr <aydinbskr@gmail.com>
2023-10-22 19:11:02 +0300

    TokenService updated.

--- Infrastructure/AYDCommerce.API.Infrastructure/Registration.cs
+++ Infrastructure/AYDCommerce.API.Infrastructure/Registration.cs
@@ -1,8 +1,10 @@
 ﻿using AYDCommerce.API.Application.Interfaces.AutoMapper;
 using AYDCommerce.API.Application.Interfaces.Tokens;
 using AYDCommerce.API.Infrastructure.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,26 @@ namespace AYDCommerce.API.Infrastructure
         {
             services.Configure<TokenSettings>(configuration.GetSection("JWT"));
             services.AddTransient<ITokenService, TokenService>();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+            {
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
         public static void AddCustomMapper(this IServiceCollection services)
         {
